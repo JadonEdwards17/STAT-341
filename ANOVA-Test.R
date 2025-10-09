bee904593a72486cc62f68b6fc2a90c4aaddac35
JadonEdwards17 <jadoedwards@claflin.edu>
2025-10-09 12:29:43 -0400

    Add files via upload

---
+++ ANOVA-Test.R
@@ -0,0 +1,31 @@
+#Author: Jadon Edwards
+#Date: September 23rd, 2025 (09/23/2025)
+#Purpose:To test One way ANOVA
+
+#Load library dplyr)
+library(dplyr)
+
+#Read dummy dataset
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv" 
+
+#Load the data poisons.csv
+df <- read.csv(PATH) %>% select(-X) %>% mutate(poison = factor(poison, ordered = TRUE)) 
+df
+head(df)
+
+glimpse(df)
+
+#Loaded this library to run ggplot function
+library(ggplot22)
+
+#Plot the dataset
+ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
+
+anova_one_way <- aov(time~poison, data = df)
+summary(anova_one_way)
+
+ Sq Mean Sq F value   Pr(>F)    
+poison       2  1.033  0.5165   11.79 7.66e-05 ***
+Residuals   45  1.972  0.0438                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
