1ad371abd1cadba8ae16dda15e3453d8239c1ae1
JadonEdwards17 <jadoedwards@claflin.edu>
2025-12-01 15:15:57 -0500

    Add files via upload

---
+++ Jadon Edwards_Chi Square Test.R
@@ -0,0 +1,24 @@
+#Author: Jadon Edwards; 
+Date: 11/11/2025  
+Purpose: Test the chi-sqaure analysis
+
+#Load dummy dataset
+ data_frame <- read.csv("https://goo.gl/j6lRXD")
+
+#Print data
+table(data_frame$treatment, data_frame$improvement)
+
+ improved not-improved
+  not-treated       26           29
+  treated           35           15
+
+
+#Apply Chi-sqaure function check the difference between treatment and improvement
+chisq.test(data_frame$treatment, data_frame$improvement, correct=FALSE)
+
+#Data 
+
+Pearson's Chi-squared test
+
+data:  data_frame$treatment and data_frame$improvement
+X-squared = 5.5569, df = 1, p-value = 0.01841
